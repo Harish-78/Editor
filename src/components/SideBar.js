d5aa35730c19799ca2d126545b9fd468224c97c1
Harish-78 <harishc0708@gmail.com>
2024-01-19 15:44:47 +0530

    side bar changes

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -7,33 +7,22 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import FolderTree, { testData } from "react-folder-tree";
 import "react-folder-tree/dist/style.css";
+import "./styles.css";
 import "react-resizable/css/styles.css";
 import { Resizable } from "react-resizable";
 import Editor from "./Editor";
-import moment from "moment"; // Import the moment library
 
 export default function ClippedDrawer() {
+  const onTreeStateChange = (state, event) => console.log(state, event);
   const [drawerWidth, setDrawerWidth] = React.useState(340);
-  const [selectedFolder, setSelectedFolder] = React.useState(null); // Added state for selected folder
-  const [editorData, setEditorData] = React.useState(""); // State to manage editor data
 
-  const onTreeStateChange = (state, event) => {
-    console.log(state, event);
-    // Set the selected folder when it's clicked
-    if (event === "select") {
-      setSelectedFolder(state.name);
-    }
+  const onResize = (event, { size }) => {
+    setDrawerWidth(size.width);
   };
-
-  const onEditorUpdate = (data) => {
-    // Update the editor data
-    setEditorData(data);
-  };
-
   const treeState = {
     name: "my-app",
-    checked: 0.5,
-    isOpen: true,
+    checked: 0.5, // half check: some children are checked
+    isOpen: true, // this folder is opened, we can see it's children
     children: [
       { name: "src", checked: 0 },
       {
@@ -55,10 +44,10 @@ export default function ClippedDrawer() {
       width="16"
       height="16"
       fill="currentColor"
-      className="bi bi-folder2-open"
+      class="bi bi-folder2-open"
       viewBox="0 0 16 16"
     >
-      {/* ... Your SVG path ... */}
+      <path d="M1 3.5A1.5 1.5 0 0 1 2.5 2h2.764c.958 0 1.76.56 2.311 1.184C7.985 3.648 8.48 4 9 4h4.5A1.5 1.5 0 0 1 15 5.5v.64c.57.265.94.876.856 1.546l-.64 5.124A2.5 2.5 0 0 1 12.733 15H3.266a2.5 2.5 0 0 1-2.481-2.19l-.64-5.124A1.5 1.5 0 0 1 1 6.14V3.5zM2 6h12v-.5a.5.5 0 0 0-.5-.5H9c-.964 0-1.71-.629-2.174-1.154C6.374 3.334 5.82 3 5.264 3H2.5a.5.5 0 0 0-.5.5V6zm-.367 1a.5.5 0 0 0-.496.562l.64 5.124A1.5 1.5 0 0 0 3.266 14h9.468a1.5 1.5 0 0 0 1.489-1.314l.64-5.124A.5.5 0 0 0 14.367 7H1.633z" />
     </svg>
   );
 
@@ -85,16 +74,8 @@ export default function ClippedDrawer() {
       >
         <Toolbar>
           <Typography variant="h6" noWrap component="div">
-            {selectedFolder ? selectedFolder : "No Folder Selected"}
+            Clipped drawer
           </Typography>
-          {selectedFolder && (
-            <Typography
-              variant="subtitle2"
-              sx={{ marginLeft: 2, opacity: 0.7 }}
-            >
-              {moment().format("MMMM Do YYYY")}
-            </Typography>
-          )}
         </Toolbar>
       </AppBar>
       <Resizable
@@ -103,7 +84,7 @@ export default function ClippedDrawer() {
         style={{ position: "relative", overflow: "hidden" }}
         minConstraints={[240, 600]}
         maxConstraints={[600, 600]}
-        onResize={(event, { size }) => setDrawerWidth(size.width)}
+        onResize={onResize}
         resizeHandles={["e"]}
         axis="x"
       >
@@ -124,8 +105,8 @@ export default function ClippedDrawer() {
               <FolderTree
                 data={treeState}
                 onChange={onTreeStateChange}
-                showCheckbox={false}
-                iconComponents={{ folderIcon: folderIcon }}
+                showCheckbox={false} // default: true
+                iconComponents={{ folderIcon: folderIcon, editIcon: editIcon }}
               />
             </div>
           </Box>
@@ -133,7 +114,7 @@ export default function ClippedDrawer() {
       </Resizable>
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
         <Toolbar />
-        <Editor data={editorData} onUpdate={onEditorUpdate} />
+        <Editor />
       </Box>
     </Box>
   );
